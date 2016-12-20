95f8493ea236db7d7f8acf3c4f9f4b1131b7b441
t00n <kelkununtel@hotmail.com>
2016-12-20 04:52:14 +0100

    [add] server creates and modifies db + publishes

--- server/ServerComponent.py
+++ server/ServerComponent.py
@@ -5,6 +5,7 @@ from autobahn.asyncio.wamp import ApplicationRunner
 from aiopg.sa import create_engine
 from psycopg2 import ProgrammingError
 import sqlalchemy as sa
+from sqlalchemy.sql import func, select
 
 from sqlalchemy_dump import dump_sql
 
@@ -14,7 +15,8 @@ orders = sa.Table('orders',
                   metadata,
                   sa.Column('id', sa.Integer, primary_key=True),
                   sa.Column('product', sa.String(255)),
-                  sa.Column('quantity', sa.Integer))
+                  sa.Column('quantity', sa.Integer),
+                  sa.Column('finished', sa.Boolean, default=False))
 
 stocks = sa.Table('stocks',
                   metadata,
@@ -30,28 +32,54 @@ async def create_table(conn, table):
 
 
 class ServerComponent(ApplicationSession):
-    async def onOrderCreate(self, order):
+    async def order_create(self, order):
+        result = await self.connection.execute(orders.insert().values(**order)
+                                                              .returning())
+        first = await result.first()
+        order['id'] = first[0]
         self.publish(u'order.oncreate', order)
-        print(order)
+        print("Created order", order)
+        return order
 
-    async def onOrderOk(self, order):
-        self.publish(u'order.onok', order)
-        print(order)
+    async def order_finish(self, order):
+        await self.connection.execute(orders
+                                      .update()
+                                      .where(orders.c.id == order['id'])
+                                      .values(finished=True))
+        self.publish(u'order.onfinish', order)
+        print("Finished order", order)
 
-    async def onStockChange(self, changes):
-        self.publish(u'stock.onchange', changes)
-        print(changes)
+    async def stock_change(self, changes):
+        await self.connection.execute(stocks.insert().values(**changes))
+        stocks_by_product = await (self
+                                   .connection
+                                   .execute(select([
+                                                   stocks.c.product,
+                                                   func.sum(stocks.c.quantity)
+                                                       .label("total")
+                                                   ])
+                                            .group_by(stocks.c.product)))
+        stocks_by_product = [dict(r) for r in stocks_by_product]
+        self.publish(u'stock.onchange', stocks_by_product)
+        print("New stock", stocks_by_product)
+        return changes
 
     async def onJoin(self, details):
         print("session ready")
-        await self.register(self.onOrderCreate, u'order.create')
-        await self.register(self.onOrderOk, u'order.ok')
-        await self.register(self.onStockChange, u'stock.change')
-        async with create_engine(user='fosdem',
-                                 database='fosdem') as engine:
-            async with engine.acquire() as conn:
-                await create_table(conn, orders)
-                await create_table(conn, stocks)
+        await self.register(self.order_create, u'order.create')
+        await self.register(self.order_finish, u'order.finish')
+        await self.register(self.stock_change, u'stock.change')
+        self.engine = await create_engine(user='fosdem',
+                                          database='fosdem')
+        self.connection = await self.engine.acquire()
+        await create_table(self.connection, orders)
+        await create_table(self.connection, stocks)
+
+        # only tests below
+        x = await self.call(u'order.create',
+                            {'product': 'leffe', 'quantity': 4})
+        await self.call(u'order.finish', x)
+        await self.call(u'stock.change', {'product': 'caca', 'quantity': 4})
 
 
 runner = ApplicationRunner(url=u"ws://localhost:8080/ws", realm=u"realm1")
