c6f31a595e0759d97b7ffa9a257676a0f9ecb8a4
t00n <kelkununtel@hotmail.com>
2016-12-20 05:53:10 +0100

    [fix] must acquire a connection for each query

--- server/ServerComponent.py
+++ server/ServerComponent.py
@@ -33,36 +33,37 @@ async def create_table(conn, table):
 
 class ServerComponent(ApplicationSession):
     async def order_create(self, order):
-        result = await self.connection.execute(orders.insert().values(**order)
-                                                              .returning())
-        first = await result.first()
-        order['id'] = first[0]
-        self.publish(u'order.oncreate', order)
-        print("Created order", order)
-        return order
+        async with self.engine.acquire() as connection:
+            result = await connection.execute(orders.insert().values(**order)
+                                                             .returning())
+            first = await result.first()
+            order['id'] = first[0]
+            self.publish(u'order.oncreate', order)
+            print("Created order", order)
 
     async def order_finish(self, order):
-        await self.connection.execute(orders
-                                      .update()
-                                      .where(orders.c.id == order['id'])
-                                      .values(finished=True))
-        self.publish(u'order.onfinish', order)
-        print("Finished order", order)
+        async with self.engine.acquire() as connection:
+            connection.execute(orders
+                               .update()
+                               .where(orders.c.id == order['id'])
+                               .values(finished=True))
+            self.publish(u'order.onfinish', order)
+            print("Finished order", order)
 
     async def stock_change(self, changes):
-        await self.connection.execute(stocks.insert().values(**changes))
-        stocks_by_product = await (self
-                                   .connection
-                                   .execute(select([
-                                                   stocks.c.product,
-                                                   func.sum(stocks.c.quantity)
-                                                       .label("total")
-                                                   ])
-                                            .group_by(stocks.c.product)))
-        stocks_by_product = [dict(r) for r in stocks_by_product]
-        self.publish(u'stock.onchange', stocks_by_product)
-        print("New stock", stocks_by_product)
-        return changes
+        async with self.engine.acquire() as connection:
+            connection.execute(stocks.insert().values(**changes))
+            stocks_by_product = await (self
+                                       .connection
+                                       .execute(select([
+                                                       stocks.c.product,
+                                                       func.sum(stocks.c.quantity)
+                                                           .label("total")
+                                                       ])
+                                                .group_by(stocks.c.product)))
+            stocks_by_product = [dict(r) for r in stocks_by_product]
+            self.publish(u'stock.onchange', stocks_by_product)
+            print("New stock", stocks_by_product)
 
     async def onJoin(self, details):
         await self.register(self.order_create, u'order.create')
@@ -70,9 +71,9 @@ class ServerComponent(ApplicationSession):
         await self.register(self.stock_change, u'stock.change')
         self.engine = await create_engine(user='fosdem',
                                           database='fosdem')
-        self.connection = await self.engine.acquire()
-        await create_table(self.connection, orders)
-        await create_table(self.connection, stocks)
+        async with self.engine.acquire() as connection:
+            await create_table(connection, orders)
+            await create_table(connection, stocks)
 
 
 runner = ApplicationRunner(url=u"ws://localhost:8080/ws", realm=u"realm1")
