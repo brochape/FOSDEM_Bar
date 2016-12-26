ef49a0b1ff5a65bae2ef737e30dec0d199ebb595
t00n <kelkununtel@hotmail.com>
2016-12-26 15:18:50 +0100

    [add] component to show an order in stock app

---
+++ components/StockOrder.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import Product from './Product.jsx';
+
+export default class StockOrder extends React.Component{
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        let products = this.props.order.products.map((s,i) => <Product name={s.name} 
+                                                                       quantity={s.quantity} />)
+        return <div>
+                    <h1>Order from {this.props.order.from}</h1>
+                    <div id = "products" >
+                        {products}
+                    </div>
+                </div>;
+    }
+}
