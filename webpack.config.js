d48e7414bd3103d218a7d8ad0f8178cf3c067981
t00n <kelkununtel@hotmail.com>
2017-01-18 23:45:50 +0100

    [enh] moved client side to the public/ dir

--- webpack.config.js
+++ webpack.config.js
@@ -2,7 +2,7 @@ module.exports = {
     entry: "./index.js",
     output: {
         path: __dirname,
-        filename: "bundle.js"
+        filename: "public/bundle.js"
     },
     module: {
         loaders: [
